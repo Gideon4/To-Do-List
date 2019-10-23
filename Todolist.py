54e28324d45efe002996c2c1be44478458566a54
Gideon4 <43590645+Gideon4@users.noreply.github.com>
2019-10-22 19:33:21 -0500

    Add files via upload

--- Todolist.py
+++ Todolist.py
@@ -1,7 +1,6 @@
 import sys
 
-d = {}
-numoitems = 0
+l = []
 
 def menu():
     choice = ""
@@ -21,28 +20,56 @@ def menu():
         menu()
 
 def showlist():
-    print(d)
+    for i in range(len(l)):
+        print(str(i+1)+": "+l[i][0]+(" "*(30-len(l[i][0])))+" Due: "+l[i][1])
 
 def add():
-    global numoitems
     newitem = []
     print("What event would you like to add?")
     newitem.append(input())
     print("When is it due?")
     newitem.append(input())
-    d[numoitems] = newitem
-    numoitems += 1
+    l.append(newitem)
     showlist()
+    save()
     menu()
 
 def delete():
     showlist()
     print("What would you like to delete?")
-    delitem = input()
-    if delitem in d:
-        del d[int(delitem)]
+    delitem = int(input())-1
+    if delitem <= len(l):
+        l.pop(delitem)
+        save()
     else:
         print("That's not an item silly!")
     menu()
 
+def save():
+    try:
+        global l
+        file = open("thelist.txt","w")
+        text = ""
+        for item in l:
+            text = text + ", ".join(item) + ";"
+        file.write(text)
+        file.close()
+    except:
+        print("Didn't work.")
+
+def load():
+    global l
+    try:
+        file = open("thelist.txt","r")
+        mytext = file.read()
+        file.close()
+        flist = mytext.split(";")
+        flist.pop()
+        for item in flist:
+            l.append(item.split(","))
+        print(l)
+    except:
+        print("Didn't work")
+    
+load()   
 menu()
